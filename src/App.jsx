669d92ee541f1f03b4d97969575f81a4b6d4a1f9
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-02-14 01:44:43 +0200

    app routing implemintation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,7 +13,13 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<p>cities</p>} />
+          <Route path='cities' element={<p>Cities</p>} />
+          <Route path='countries' element={<p>countries</p>} />
+          <Route path='form' element={<p>form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import AppNav from '../components/AppNav';
-sm
+import Map from '../components/Map';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css'
+
 
 export default function AppLayout() {
     return (
-        <div>
-            <AppNav />
+        <div className={styles.app}>
+            <Sidebar />
+            <Map />
         </div>
     )
 }
